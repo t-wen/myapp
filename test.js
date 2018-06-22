74254dbbf78ac4c3bb89f6d0650286ede3230004
t-wen <30753500+t-wen@users.noreply.github.com>
2018-06-23 00:49:54 +0800

    update

--- test.js
+++ test.js
@@ -1,29 +1,42 @@
-var mysql  = require('mysql');  
- 
-var connection = mysql.createConnection({     
-  host     : 'localhost',       
-  user     : 'root',              
-  password : '661475chen',       
-  port: '3306',                   
-  database: 'my_db', 
-}); 
- 
-connection.connect();
+var express = require('express');
+var app = express();
 
-var response = {
-    WechartName : 'gggg',
-    PhoneNumber : '12345678',
-    Password    : '12345678'
-};
+app.use(express.static( __dirname + '/html'));
+app.get('/signup', function (req, res) {
+    var mysql  = require('mysql');  
+    
+    var connection = mysql.createConnection({     
+    host     : 'localhost',       
+    user     : 'root',              
+    password : '661475chen',       
+    port: '3306',                   
+    database: 'my_db', 
+    }); 
+    
+    connection.connect();
 
-var  addSql = 'INSERT INTO Persons(WechartName, PhoneNumber, Password) VALUES(?,?,?)';
-var  addSqlParams = ['response.WechartName', 'response.PhoneNumber', 'response.Password'];
+    var response = {
+        "WechartName" : req.query.WechartName,
+        "PhoneNumber" : req.query.PhoneNumber,
+        "Password"    : req.query.Password
+    };
 
-connection.query(addSql,addSqlParams,function (err, result) {
-        if(err){
-            console.log('INSERT ID:',result); 
-            return;
-        }        
-});
- 
-connection.end();
+    var  addSql = 'INSERT INTO Persons(WechartName, PhoneNumber, Password) VALUES(?,?,?)';
+    var  addSqlParams = [response.WechartName, response.PhoneNumber, response.Password];
+
+    connection.query(addSql,addSqlParams,function (err, result) {
+            if(err){
+                console.log('INSERT ID:',err.message);
+                res.send('执行出错'); 
+                return;
+            }
+            res.send('Finish signup!');        
+    });
+    
+    connection.end();
+})
+var server = app.listen(8082, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
